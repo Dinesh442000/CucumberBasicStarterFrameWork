741740c8235478edfcd65195637b68e7871e7fd5
Dinesh442000 <dchoudhary442000@gmail.com>
2024-12-19 07:19:12 +0530

    reporting

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -7,7 +7,8 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions"
 ,monochrome = true, tags = "@PlaceOrder or @SearchOrder", 
-plugin = {"html:target/cucumber.html", "json:target/cucumber.json"}
+plugin = {"html:target/cucumber.html", "json:target/cucumber.json",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 public class TestNGTestRunner extends AbstractTestNGCucumberTests  {
 	
