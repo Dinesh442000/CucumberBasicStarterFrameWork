e9a9342c5dfe5755333cae3de45d75ad57435704
Dinesh442000 <dchoudhary442000@gmail.com>
2024-12-19 07:51:00 +0530

    SS and reporting attached

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,8 +1,18 @@
 package stepDefinitions;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+
+
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Scenario;
 import utils.TestContextSetup;
 
 public class Hooks {
@@ -16,4 +26,15 @@ public class Hooks {
 	public void afterScenario() throws IOException {
 		testConceptSetup.testBase.webDriverManager().quit();;
 	}
+	
+	@AfterStep
+	public void getScreenShotOnFail(Scenario scenario) throws IOException {
+		WebDriver driver = testConceptSetup.testBase.webDriverManager();
+		if(scenario.isFailed()) {
+			File srcScreenShot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			byte[] fileContent = FileUtils.readFileToByteArray(srcScreenShot);
+			scenario.attach(fileContent, "image/png", "image");
+		}
+	}
+	
 }
